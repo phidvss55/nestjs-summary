59889256811f853b5267caf0c630068de6f64c60
Phi Dinh <phidv9855@gmail.com>
2023-05-25 11:34:29 +0700

    add dataset

--- mongo.sql
+++ mongo.sql
@@ -9,11 +9,54 @@ db.students.insertMany( [
    { sID: 17301, name: "harley", year: 6, score: 3.1 },
 ] )
 
+db.inventory.insertMany( [
+   { item: "journal", instock: [ { warehouse: "A", qty: 5 }, { warehouse: "C", qty: 15 } ] },
+   { item: "notebook", instock: [ { warehouse: "C", qty: 5 } ] },
+   { item: "paper", instock: [ { warehouse: "A", qty: 60 }, { warehouse: "B", qty: 15 } ] },
+   { item: "planner", instock: [ { warehouse: "A", qty: 40 }, { warehouse: "B", qty: 5 } ] },
+   { item: "postcard", instock: [ { warehouse: "B", qty: 15 }, { warehouse: "C", qty: 35 } ] }
+]);
+
+-- QUERY DOCUMENT
+db.students.find()
+db.students.find({ score: { $lt: 3  }})
+db.students.find({ $or: [{ year: 1 }, { score: { $gt: 3 } }]})
+
+db.inventory.find( { "instock": { warehouse: "A", qty: 5 } } )
+db.inventory.find( { item: null } ) -- either item is null or not exist item field 
+db.inventory.find( { item : { $exists: false } } ) -- not contain item field
+
+
 -- UPDATE
-db.my_collections.updateOne({ name: 'Student' }, { ... $set: { name: 'Student one' } })
+db.students.updateOne({ name: 'Student' }, { $set: { name: 'Student one' } })
+db.students.updateOne( { _id: 3 }, [ { $set: { "test3": 98, modified: "$$NOW"} } ] )
+db.students2.updateMany( {},
+  [
+    { $replaceRoot: { newRoot:
+       { $mergeObjects: [ { quiz1: 0, quiz2: 0, test1: 0, test2: 0 }, "$$ROOT" ] }
+    } },
+    { $set: { modified: "$$NOW"}  }
+  ]
+)
+--
+db.inventory.updateOne(
+   { item: "paper" },
+   {
+     $set: { "size.uom": "cm", status: "P" },
+     $currentDate: { lastModified: true }
+   }
+)
+db.inventory.replaceOne(
+   { item: "paper" },
+   { item: "paper", instock: [ { warehouse: "A", qty: 60 }, { warehouse: "B", qty: 40 } ] }
+)
 
 
 -- DELETE
-db.students.aggregate(
-  { $filter: { name : { $eq : "alex" } } }
-);
+db.orders.deleteOne( { "_id" : ObjectId("563237a41a4d68582c2509da") } )
+
+db.inventory.deleteMany({}) -- Delete all the document
+db.inventory.deleteMany({ status : "A" })
+-- db.students.aggregate(
+--   { $filter: { name : { $eq : "alex" } } }
+-- );
